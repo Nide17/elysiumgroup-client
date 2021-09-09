2ab4ce809a1638ba2e057cbab8ca18b1a20a87ac
Nide17 <nidedrogba@gmail.com>
2021-09-09 19:57:52 +0200

    gallery bug

--- src/components/projects/All.js
+++ src/components/projects/All.js
@@ -2,92 +2,80 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { setProjects } from "../../redux/projects/projects.actions";
 
-const All = (props) => {
+const All = ({ projects, setProjects }) => {
 
   useEffect(() => {
     // Inside this callback function we perform our side effects.
-    props.setProjects();
+    setProjects();
   });
 
-  const allProjects = props.projects.map((proj) => {
+  return (
+    <div className="row">
 
-    const { id, pName, pKey, pLocation, pGallery, pDescription } = proj;
+      {projects.map((proj) =>
 
-    return (
+        <div className="col-12 col-sm-6 col-xl-3 mb-3" key={proj.id}>
 
-      <div className="col-12 col-sm-6 col-xl-3 mb-3" key={id}>
+          <div className="card card-body bg-light" id={proj.pClient.split(' ').join('-') + proj.id}>
+            <img
+              className="card-img-top img-thumbnail rounded"
+              src={proj.pGallery[0]}
+              alt="Elysium Group Ltd"
+            />
 
-        <div className="card card-body bg-light" id={pKey}>
-          <img
-            className="card-img-top img-thumbnail rounded"
-            src={pGallery[0]}
-            alt="Elysium Group Ltd"
-          />
+            <div
+              className="card-img-overlay"
+            >
+              <button type="button" className="btn btn-outline-warning btn-sm" data-toggle="modal" data-target={`.${proj.pClient.split(' ').join('-') + proj.id}`}>
+                View more ...
+              </button>
+            </div>
 
-          <div
-            className="card-img-overlay"
-          >
-            <button type="button" className="btn btn-outline-warning btn-sm" data-toggle="modal" data-target={`.${pKey}`}>
-              View more ...
-            </button>
+            <div className="project-info p-2 text-center text-uppercase">
+              <h6>{proj.pName}</h6>
+              <p>{proj.pLocation}</p>
+            </div>
           </div>
 
-          <div className="project-info p-2 text-center text-uppercase">
-            <h6>{pName}</h6>
-            <p>{pLocation}</p>
-          </div>
-        </div>
+          {/* MODAL */}
+          <div className={`modal fade ${proj.pClient.split(' ').join('-') + proj.id}`} tabIndex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
 
-        {/* MODAL */}
-        <div className={`modal fade ${pKey}`} tabIndex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+            <div className="modal-dialog modal-xl">
+              <div className="modal-content">
 
-          <div className="modal-dialog modal-xl">
-            <div className="modal-content">
+                <div className="container page-top">
 
-              <div className="container page-top">
+                  <div className="row img-row">
 
-                <div className="row img-row">
-
-                  {/* iteration */}
-                  {pGallery.map(pImage => {
-
-                    return (
+                    {/* iteration */}
+                    {proj.pGallery.map(pImage => (
                       <div className="col-lg-3 col-md-4 col-xs-6 thumb" key={pImage} onClick={(e) => e.preventDefault()}>
                         <a href={pImage} className="fancybox" id="fancybox" rel="ligthbox">
                           <img src={pImage} className="zoom img-fluid" alt="" />
                         </a>
                       </div>
-                    )
+                    ))}
 
-                  })}
+                  </div>
 
-                </div>
+                  <div className="row desc">
 
-                <div className="row desc">
+                    <div className="col-12 text-center">
+                      <h2>{proj.pName}</h2>
+                      {proj.pDescription}
+                    </div>
 
-                  <div className="col-12 text-center">
-                    <h2>{pName}</h2>
-                    {pDescription}
                   </div>
 
                 </div>
 
               </div>
-
             </div>
+
           </div>
 
-        </div>
-
-      </div>
-
-
-    );
-
-  });
-
-  return <div className="row">
-    {allProjects}</div>;
+        </div>)}
+    </div>)
 
 };
 
