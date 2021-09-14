0eeb17ca8b4c43511ae447cf07a23d3076162be1
Nide17 <nidedrogba@gmail.com>
2021-09-14 05:19:56 +0200

    single profile css

--- src/components/team/ViewBio.js
+++ src/components/team/ViewBio.js
@@ -14,24 +14,27 @@ const ViewBio = ({ setMembers, members }) => {
 
     return (
         <section className="about-section py-0 py-lg-4">
-        
+
             <div className="container about-container">
 
-                        <>
-                            <div className="row">
-                                <div className="col-12">
-                                    <h2 className="text-center py-3 font-weight-bolder w-100">{memberToView && memberToView.memberName}</h2>
-                                </div>
-                            </div>
+                <div className="row">
 
-                            <div className="row">
-                                <div className="">
-                                    <div className="card-body">
-                                    <p>{memberToView && memberToView.memberDescription}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </>
+                    <div className="col-12 col-sm-4 memberImg d-flex align-items-center justify-content-center">
+                        <img className="w-100" src={memberToView && memberToView.memberImage} alt={memberToView && memberToView.memberName} />
+                    </div>
+
+                    <div className="col-12 col-sm-8">
+                        <h2 className="text-center py-3 font-weight-bolder w-100">{memberToView && memberToView.memberName}</h2>
+
+                        <h4>{memberToView && memberToView.designation}</h4>
+                        <h6>{memberToView && memberToView.memberEducation}</h6>
+
+                        <div className="profile-desc mt-lg-4">
+                            <p>{memberToView && memberToView.memberDescription}</p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </section>
     )
