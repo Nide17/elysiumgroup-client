3686fc7d85397bd2493fa70f616ba1a81f50812a
Nide17 <nidedrogba@gmail.com>
2021-09-17 15:03:43 +0200

    services

--- src/redux/services/servicesData.js
+++ src/redux/services/servicesData.js
@@ -6,60 +6,89 @@ import electrical1 from '../../images/electrical1.jpg'
 import accounting from '../../images/accounting.jpg'
 import web1 from '../../images/web1.jpg'
 import plumbing from '../../images/plumbing.jpg'
+import building1 from '../../images/building.svg'
 
 const sData = [
     {
         id: 0,
-        sName: 'Architecture & Design',
+        sName: 'Transportation Planning and Feasibility Study',
         sTitle: "arch",
         sSrc: architecture,
         sDetails: 'As experienced Architects and Designers, we deliver excellent services related to the designs of building, bridges, roads, railways, residential houses, water routes and so on. We provide consultancy services for reasonable prices.'
     },
     {
         id: 1,
-        sName: 'Building & Construction',
+        sName: 'Topographic Surveying and Design',
         sTitle: "eng",
         sSrc: building,
         sDetails: 'Elysium Group engineers deliver excellent services related to the constructiom of small scale and large buildings and houses. We do related quantity surveying professionally with up to date technologies. In addition, We offer building completion and finishing services. we also provide consultancy services for reasonable prices.'
     },
     {
         id: 2,
-        sName: 'Roads & Railway Activities',
+        sName: 'Hydraulic and Structural Engineering Design',
         sTitle: "road",
         sSrc: road2,
         sDetails: 'At Elysium Group, we provide also excellent roads and railways construction services. We also provide quantity surveying services related with roads and railways professionally. We also have a consultancy team that can help you with your projects and ideas.'
     },
     {
         id: 3,
-        sName: 'Site Preparation Activities',
+        sName: 'Geotechnical Engineering, Material Testing and Pavement Design',
         sTitle: "site",
         sSrc: siteprep,
         sDetails: 'Site preparation is vital to any construction activity as it gives a clear picture of what is going to be built. As a solution, we have advanced tools and materials that can help with the preparation of any site or place to put your building, road and so on. We also provide the full service and labours for that.'
     },
     {
         id: 4,
-        sName: 'Electrical & Power Activities',
+        sName: 'Management of Civil Engineering Construction',
         sTitle: "elec",
         sSrc: electrical1,
         sDetails: 'As every modern building must have electrical power, We provide services related to the power and electrical engineering via experienced engineers with professionalism. We repair damaged electrical installation and of course we install electricity at on buildings, houses, roads and any infrastructure that may require electrical power.'
     },
     {
         id: 5,
-        sName: 'Water & Plumbing Activities',
+        sName: 'Building Design and Supervision',
         sTitle: "water",
         sSrc: plumbing,
         sDetails: 'We provide services related but not limited with water collection, treatment and supply. We repair and plan new water sources and routes.'
     },
     {
         id: 6,
-        sName: 'Accounting & Consultancy',
+        sName: 'Environmental Engineering',
         sTitle: "acc",
         sSrc: accounting,
         sDetails: 'We offer accounting, bookkeeping and secretary services that have to lead your business, industry or organization to a long term benefit. We deliver tax advisory services, auditing services, logistics services and we can help you with market research and targetting.'
     },
     {
         id: 7,
-        sName: 'Web Development Services',
+        sName: 'Preparation of Engineering Cost Estimate',
+        sTitle: "web",
+        sSrc: web1,
+        sDetails: 'We design and develop beautiful websites for businesses, individuals and organizations at a reasonable prices. These websites help you to generate new clients easily. We also maintain your websites and additionally, we renew old websites as well as correcting websites developed with errors.'
+    },
+    {
+        id: 8,
+        sName: 'Preparation of Tender Document',
+        sTitle: "web",
+        sSrc: web1,
+        sDetails: 'We design and develop beautiful websites for businesses, individuals and organizations at a reasonable prices. These websites help you to generate new clients easily. We also maintain your websites and additionally, we renew old websites as well as correcting websites developed with errors.'
+    },
+    {
+        id: 9,
+        sName: 'Accounting, bookkeeping and auditing activities, tax consultancy',
+        sTitle: "web",
+        sSrc: web1,
+        sDetails: 'We design and develop beautiful websites for businesses, individuals and organizations at a reasonable prices. These websites help you to generate new clients easily. We also maintain your websites and additionally, we renew old websites as well as correcting websites developed with errors.'
+    },
+    {
+        id: 10,
+        sName: 'Business Development, Management and Market research and public opinion polling consultancy',
+        sTitle: "web",
+        sSrc: web1,
+        sDetails: 'We design and develop beautiful websites for businesses, individuals and organizations at a reasonable prices. These websites help you to generate new clients easily. We also maintain your websites and additionally, we renew old websites as well as correcting websites developed with errors.'
+    },
+    {
+        id: 11,
+        sName: 'ICT Consultancy and web Development Services',
         sTitle: "web",
         sSrc: web1,
         sDetails: 'We design and develop beautiful websites for businesses, individuals and organizations at a reasonable prices. These websites help you to generate new clients easily. We also maintain your websites and additionally, we renew old websites as well as correcting websites developed with errors.'
