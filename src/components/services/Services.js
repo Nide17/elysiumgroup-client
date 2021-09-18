8028f99b21a9788eaeab3dc5dbb9d02223340f19
Nide17 <nidedrogba@gmail.com>
2021-09-18 07:16:51 +0200

    services carousel

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption, ListGroup, ListGroupItem } from 'reactstrap';
 import { connect } from "react-redux";
 import { setServices } from "../../redux/services/services.actions";
-// import building1 from '../../images/building.svg'
-// import civil1 from '../../images/civil1.jpg'
 
 const Services = ({ setServices, services }) => {
 
@@ -11,36 +9,18 @@ const Services = ({ setServices, services }) => {
     setServices();
   }, [setServices]);
 
-  // const items = [
-  //   {
-  //     src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-  //     sTitle: 'Slide 1',
-  //     caption: 'Slide 1'
-  //   },
-  //   {
-  //     src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-  //     sTitle: 'Slide 2',
-  //     caption: 'Slide 2'
-  //   },
-  //   {
-  //     src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-  //     sTitle: 'Slide 3',
-  //     caption: 'Slide 3'
-  //   }
-  // ];
-
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
 
   const next = () => {
     if (animating) return;
-    const nextIndex = activeIndex === services && services.length - 1 ? 0 : activeIndex + 1;
+    const nextIndex = activeIndex === services.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   }
 
   const previous = () => {
     if (animating) return;
-    const nextIndex = activeIndex === 0 ? services && services.length - 1 : activeIndex - 1;
+    const nextIndex = activeIndex === 0 ? services.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   }
 
@@ -49,12 +29,12 @@ const Services = ({ setServices, services }) => {
     setActiveIndex(newIndex);
   }
 
-  const slides = services && services.map((servic) => {
+  const slides = services && services.map((servic, index) => {
     return (
       <CarouselItem
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
-        key={servic.sSrc}
+        key={index}
       >
         <img src={servic.sSrc} alt={servic.sTitle} />
         {/* <CarouselCaption captionText={servic.sName} captionHeader={servic.sName} /> */}
@@ -93,8 +73,8 @@ const Services = ({ setServices, services }) => {
           <div className="col-12 col-sm-6 pl-1">
 
             <ListGroup>
-              {services && services.map((servic) =>
-                <ListGroupItem color="info" className="text-dark font-weight-bolder text-uppercase py-2">
+              {services && services.map((servic, index) =>
+                <ListGroupItem key={index} color="secondary" className="text-dark font-weight-bolder text-uppercase py-2">
                   {servic.sName}
                 </ListGroupItem>)}
             </ListGroup>
